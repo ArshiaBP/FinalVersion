1b60d6cf692bdd29c20ccbd62f64d5f0fdd9c418
ARSHIABP <ARSHIA.BAHAR@hotmail.com>
2022-07-02 23:01:46 +0430

    Just the last version of the project

---
+++ src/RunProgram.java
@@ -0,0 +1,6 @@
+public class RunProgram {
+    public static void main(String[] args) throws Exception {
+        Client client = new Client();
+        client.connect();
+    }
+}
---
+++ src/VoiceMessages.java
@@ -0,0 +1,18 @@
+import java.io.Serializable;
+
+/**
+ * The type Voice messages.
+ */
+public class VoiceMessages extends Chat implements Serializable {
+
+    /**
+     * Instantiates a new Voice messages.
+     *
+     * @param id            the id
+     * @param servergroupId the servergroup id
+     * @param name          the name
+     */
+    public VoiceMessages(int id , int servergroupId , String name) {
+        super(id , servergroupId , name);
+    }
+}
